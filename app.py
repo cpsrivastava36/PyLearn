c5fb790064fb29e701c62295053896897044c05b
chandra srivastava <cp.srivastava36@gmail.com>
2024-11-02 12:56:03 +0530

    initial commit for simple Py learn files

---
+++ app.py
@@ -0,0 +1,26 @@
+print('Hello World')
+print("Hello World")
+print(f'Hey')
+a=None
+print(a)
+print('type of a is',type(a))
+b=(10,2)
+print(b)
+str:"Hellow World" "Python Porgram"
+print(str)
+print(47//7)
+c="10"
+d="11"
+print(int(c)+int(d))
+string="Hi i am good boy"
+for char in string:
+    print(char)
+name="Harry"
+print(name[-4:-2])
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+
+if(a>18):
+    print("You can drive")
+else:
+    print("You can't drive")

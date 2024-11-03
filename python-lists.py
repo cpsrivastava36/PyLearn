db583cefdd3c115b6f2842f6455a7a0ce5f50630
chandra srivastava <cp.srivastava36@gmail.com>
2024-11-03 17:51:30 +0530

    updating repo with list in-built functions

--- python-lists.py
+++ python-lists.py
@@ -46,3 +46,45 @@ print(namesWith_0)
 names = ["Priya", "Daivik", "Sachin", "Sehwag", "Prateek", "Deepu"]
 namesWith_4 = [ name for name in names if len(name)>6]
 print(namesWith_4)
+
+#list methods
+#1. append. the original method get modified
+list=[1,2,3,45,87,9]
+list.append("HARRY")
+print(list)
+
+#2.reverse()
+list.reverse()
+print(list)
+
+#3.sort(reverse=True) // it only supports for cases where consistent items of same type are in list. Heterogeneous items in list can not ber reversed by this 
+#method. you need to use  reverse() method for that.
+#list.sort(reverse=True)
+list1=[1,4,67,90]
+print(list1.sort(reverse=True)) # this one doesnt print anything
+print(list1)
+
+#4. index gives element atthat index and count gives number of times that element was repeated
+print(list1.count(67))
+
+#5. copy method creates shallow copy of the same list and its copy by value and deosnt impact orginal list
+list2=list1.copy()
+print(list2)
+
+#6. copy by brefernce is done by quals operator and it is deep copy of original list and any changes in copied list impacts the original list as well.
+list3=list2
+print(list3)
+list3.append(22)
+print("original list impacted ",list2)
+
+#7. concatenate two list by using + operator
+list4 = list3+list
+print(list4)
+
+#8. add alist to a list
+colors=["violet","green","blue","yellow"]
+rainbow=["violet","indigo","orange"]
+colors.extend(rainbow)
+print(colors)
+
+
